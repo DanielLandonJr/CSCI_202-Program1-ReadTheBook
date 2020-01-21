28856918a9b8fa3ffb1611f92d2b2e70774778ac
Daniel C. Landon Jr <daniel.landonjr@gmail.com>
2020-01-21 18:39:59 -0500

    Read file, counted lines and words

--- src/app/ProcessTheBook.java
+++ src/app/ProcessTheBook.java
@@ -1,6 +1,8 @@
 package app;
 
+import java.io.DataInput;
 import java.io.File;
+import java.util.Scanner;
 
 public class ProcessTheBook {
     
@@ -8,6 +10,27 @@ public class ProcessTheBook {
 
         try {
             File readFile = new File("oliver.txt");
+
+            Scanner _dataInput = new Scanner(readFile);
+
+            int _lineCounter = 0;
+            int _wordCounter = 0;
+            
+            while(_dataInput.hasNext()){
+                String _line = _dataInput.nextLine();
+                String[] _parse;
+                
+                _parse = _line.split(" ");
+
+                _wordCounter+= _parse.length;
+                _lineCounter++;
+            } // end while
+
+            _dataInput.close();
+
+            System.out.println("Number of lines read: " + _lineCounter);
+            System.out.println("Number of words read: " + _wordCounter);
+
         } // end try
 
         catch (Exception e) {
@@ -16,6 +39,7 @@ public class ProcessTheBook {
         } // end catch
 
         finally {
+
             System.out.println("Hello There");
         }
 
